1474a4a024f0ec69eb06c5519eb07f8245020570
JessicaNakayu <122330659+JessicaNakayu@users.noreply.github.com>
2023-02-01 18:19:41 -0700

    clone 1

--- python_strings.py
+++ python_strings.py
@@ -26,10 +26,15 @@ current_year = 2023
 # TODO String Indexing
 #   - Print the following items (one per line) (print using variables)
 #       - first name  
+print(my_first_name)
 #       - last name
+print(my_last_name)
 #       - first letter of your first name (use the +index)
+print(my_first_name[0])
 #       - second letter of your last name (use the -index)
+print(my_last_name[-5])
 #       - first two letter of your first name (use the +index)
+print(my_first_name[0,1])
 #       - second two letter of your last name (use the -index)
 
 
@@ -54,11 +59,18 @@ current_year = 2023
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
 #       - possesive first name -birth year is- year of birth 
+print(my_first_name + '\'s' + " birth year is " + str(my_year_of_birth))
+
 #       - tab last name current year
+print("\t", my_last_name, current_year)
+
 
 
 # TODO String methods
 #   - Print the following items (one per line) (print using variables)
 #       - first name and last name in lower case
+print(my_first_name.casefold(), my_last_name.casefold())
 #       - length of last name
+print(len(my_last_name))
 #       - first name and last name all in upper case
+print(my_first_name.upper(), my_last_name.upper())
